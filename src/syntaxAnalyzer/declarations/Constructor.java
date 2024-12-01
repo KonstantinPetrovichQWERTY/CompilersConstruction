fb9785e3e4bd3fb1162a1fb93201b8709df70115
iliyasone <iliyas.dzabbarov@gmail.com>
2024-12-02 02:10:33 +0300

    refactoring

--- src/syntaxAnalyzer/declarations/Constructor.java
+++ src/syntaxAnalyzer/declarations/Constructor.java
@@ -8,7 +8,7 @@ import syntaxanalyzer.utils.ParameterDeclaration;
 
 public class Constructor extends Declaration {
 
-    private Block body;
+    private Block body = new Block();
     private Parameters parameters;
     public List<ParameterDeclaration> getParameters() {
         return parameters.getParameters();
@@ -22,22 +22,14 @@ public class Constructor extends Declaration {
     public Integer parse(List<Token> tokens, Integer index) {
         ensureToken(tokens, index, TokenType.KEYWORD_THIS);
         index+=1;
-
         ensureToken(tokens, index, TokenType.PUNCTUATION_LEFT_PARENTHESIS);
-
         parameters = new Parameters();
         index = parameters.parse(tokens, index);
-
         ensureToken(tokens, index, TokenType.PUNCTUATION_RIGHT_PARENTHESIS);
-
         index += 1;
         ensureToken(tokens, index, TokenType.KEYWORD_IS);
-
         index += 1;
-        // Parse method body (redirect to Block)
-        body = new Block();
         index = body.parse(tokens, index);
-
         ensureToken(tokens, index, TokenType.KEYWORD_END);
         return index;
     }
