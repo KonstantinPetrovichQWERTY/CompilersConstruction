5229a1831456a6868f420f3c3cc47709c890c8e5
iliyasone <iliyas.dzabbarov@gmail.com>
2024-12-04 03:02:21 +0300

    ExpressionSuffix: Makes array of arguments optional, to explicitly distinct between method call with no args and field

--- src/syntaxanalyzer/utils/ExpressionSuffix.java
+++ src/syntaxanalyzer/utils/ExpressionSuffix.java
@@ -1,12 +1,14 @@
-package syntaxanalyzer.declarations;
+package syntaxanalyzer.utils;
 
 import lexicalanalyzer.Token;
+import syntaxanalyzer.declarations.Expression;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @param identifier The method or field name after the '.'
  * @param arguments  Null if it's a field access, or a list of expressions if it's a method call
  */
-public record ExpressionSuffix(Token identifier, List<Expression> arguments) {
+public record ExpressionSuffix(Token identifier, Optional<List<Expression>> arguments) {
 }
