8ae74af0d178e560d25e8b83f0466bbc81eff34e
KonstantinPetrovichQWERTY <skatkov24@mail.ru>
2024-12-04 05:16:40 +0300

    пробегаем все тесты

--- src/syntaxanalyzer/SyntaxAnalyzer.java
+++ src/syntaxanalyzer/SyntaxAnalyzer.java
@@ -4,32 +4,43 @@ import java.util.List;
 import lexicalanalyzer.LexicalAnalyzer;
 import lexicalanalyzer.Token;
 import syntaxanalyzer.declarations.Cls;
-import syntaxanalyzer.declarations.Constructor;
-import syntaxanalyzer.declarations.Method;
-import syntaxanalyzer.declarations.Variable;
 
 public class SyntaxAnalyzer {
     public static void main(String[] args) {
-        List<Token> tokens = LexicalAnalyzer.getTokens("src/test/testOLang/test.o");
+        
+        String[] files = {"arrayInitializationAccess.o", "booleanOperations.o", "classDeclarationInstantiation.o", "complexExpression.o", "constructorDeclaration.o", "ifStatment.o", "inheritance.o", "integerOperations.o", "listDeclaration.o", "methodDeclarationCall.o", "methodsOverriding.o", "realOperations.o", "returnStatementExpression.o", "test.o", "varDeclarationAssignment.o", "whileLoop.o"};
 
-        AST rootNode = new AST();
-        List<Cls> classes = rootNode.parse(tokens);
+        for (String elem : files){
+            System.out.println("\n" + elem);
 
-        for (Cls cls : classes) {
-            System.out.println("CLASS " + cls.getName());
-            System.out.println("BASECLASS " + cls.getBaseClass());
-
-            for (Constructor constructor : cls.getBody().getConstructors()) {
-                System.out.println("CONSTRUCTOR " + constructor.getParameters());
+            String p = "src/test/testOLang/" + elem;
+            List<Token> tokens = LexicalAnalyzer.getTokens(p);
+            
+            try {
+                AST rootNode = new AST();
+                List<Cls> classes = rootNode.parse(tokens); 
+            } catch (Exception e) {
+                System.out.println("Произошла ошибка: " + e.getMessage());
+                continue;
             }
+       
+            
+            // for (Cls cls : classes) {
+            //     System.out.println("CLASS " + cls.getName());
+            //     System.out.println("BASECLASS " + cls.getBaseClass());
 
-            for (Variable var : cls.getBody().getVariables()) {
-                System.out.println("VARIABLE " + var.getName() + " " + var.getExpression().getPrimary().getPrimaryType().toString());
-            }
+            //     for (Constructor constructor : cls.getBody().getConstructors()) {
+            //         System.out.println("CONSTRUCTOR " + constructor.getParameters());
+            //     }
 
-            for (Method method : cls.getBody().getMethods()) {
-                System.out.println("METHOD "  + method.getName() + " " + method.getParameters());
-            }
+            //     for (Variable var : cls.getBody().getVariables()) {
+            //         System.out.println("VARIABLE " + var.getName() + " " + var.getExpression().getPrimary().getPrimaryType().toString());
+            //     }
+
+            //     for (Method method : cls.getBody().getMethods()) {
+            //         System.out.println("METHOD "  + method.getName() + " " + method.getParameters());
+            //     }
+            // }
         }
     }   
 
